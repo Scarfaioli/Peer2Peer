7d69924a8549c03825f1d00d703c72da61d769c5
Samuel Faioli <scarf1999@gmail.com>
2023-11-23 14:28:34 -0300

    .

--- Peer.java
+++ Peer.java
@@ -1,6 +1,13 @@
-public class Peer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args){
-        BufferedReader
+public class Peer {
+    public static void main(String[] args) throws IOException {
+        //Recebe valores do teclado para nome de usuario e porta para a conexão
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("> insira o nome do user e porta # para este peer:");
+        //Remove os espaços visando ter um array de duas posições [0] username e [1] porta da comunicação
+        String[] stupValues = bufferedReader.readLine().split(" ");
     }
 }
